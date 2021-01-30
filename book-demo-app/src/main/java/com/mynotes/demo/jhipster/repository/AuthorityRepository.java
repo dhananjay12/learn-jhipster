678dde7fbd25e22f7a9e0f9f7cae8fc109a27a1f
dhananjay12 <dhananjay1204@gmail.com>
2021-01-30 02:31:04 +0100

    Initial version of bookapp generated by JHipster-6.10.5

---
+++ book-demo-app/src/main/java/com/mynotes/demo/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mynotes.demo.jhipster.repository;
+
+import com.mynotes.demo.jhipster.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ book-demo-app/src/main/java/com/mynotes/demo/jhipster/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.mynotes.demo.jhipster.web.rest;
